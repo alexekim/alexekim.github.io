e396b26791a902c2a8d985fbe61955cc7e85069e
Alex Kim <alexander.e.kim@gmail.com>
2016-03-10 11:40:27 -0600

    added keyboard listeners. no direct input yet..

--- scripts/app.js
+++ scripts/app.js
@@ -2,110 +2,110 @@ $(document).ready(function(){
   console.log("the window is loaded")
         // All keyboard sound functions here
 
-        var melodyOne = document.createElement('audio');
-        melodyOne.setAttribute('src', 'media/melody1.wav');
-        //audioElement.load()
-        $.get();
-        melodyOne.addEventListener("load", function() {
-            melodyOne.play();
-        }, true);
-        $(".sample").click(function() {
-            melodyOne.play();
-        });
-
-        var cAudioElement = document.createElement('audio');
-        cAudioElement.setAttribute('src', 'media/c.wav');
-        //audioElement.load()
-        $.get();
-        cAudioElement.addEventListener("load", function() {
-            cAudioElement.play();
-        }, true);
-        $('.c').click(function() {
-            cAudioElement.play();
-        });
-        // $('.d').click(function() {
-        //     audioElement.pause();
-        // });
-        var dAudioElement = document.createElement('audio');
-        dAudioElement.setAttribute('src', 'media/d.wav');
-        //audioElement.load()
-        $.get();
-        dAudioElement.addEventListener("load", function() {
-            dAudioElement.play();
-        }, true);
-        $('.d').click(function() {
-            dAudioElement.play();
-        });
-
-        var eAudioElement = document.createElement('audio');
-        eAudioElement.setAttribute('src', 'media/e.wav');
-        //audioElement.load()
-        $.get();
-        eAudioElement.addEventListener("load", function() {
-            eAudioElement.play();
-        }, true);
-        $('.e').click(function() {
-            eAudioElement.play();
-        });
-
-        var fAudioElement = document.createElement('audio');
-        fAudioElement.setAttribute('src', 'media/f.wav');
-        //audioElement.load()
-        $.get();
-        fAudioElement.addEventListener("load", function() {
-            fAudioElement.play();
-        }, true);
-        $('.f').click(function() {
-            fAudioElement.play();
-        });
-
-        var gAudioElement = document.createElement('audio');
-        gAudioElement.setAttribute('src', 'media/g.wav');
-        //audioElement.load()
-        $.get();
-        gAudioElement.addEventListener("load", function() {
-            gAudioElement.play();
-        }, true);
-        $('.g').click(function() {
-            gAudioElement.play();
-        });
-
-        var aAudioElement = document.createElement('audio');
-        aAudioElement.setAttribute('src', 'media/a.wav');
-        //audioElement.load()
-        $.get();
-        aAudioElement.addEventListener("load", function() {
-            aAudioElement.play();
-        }, true);
-        $('.a').click(function() {
-            aAudioElement.play();
-        });
-
-        var bAudioElement = document.createElement('audio');
-        bAudioElement.setAttribute('src', 'media/b.wav');
-        //audioElement.load()
-        $.get();
-        bAudioElement.addEventListener("load", function() {
-            bAudioElement.play();
-        }, true);
-        $('.b').click(function() {
-            bAudioElement.play();
-        });
-
-        var c1AudioElement = document.createElement('audio');
-        c1AudioElement.setAttribute('src', 'media/c1.wav');
-        //audioElement.load()
-        $.get();
-        c1AudioElement.addEventListener("load", function() {
-            c1AudioElement.play();
-        }, true);
-        $('.c1').click(function() {
-            c1AudioElement.play();
-        });
 
 });
 // END OF DOC READY
 
+var melodyOne = document.createElement('audio');
+melodyOne.setAttribute('src', 'media/melody1.wav');
+//audioElement.load()
+$.get();
+melodyOne.addEventListener("load", function() {
+    melodyOne.play();
+}, true);
+$(".sample").click(function() {
+    melodyOne.play();
+});
+
+var cAudioElement = document.createElement('audio');
+cAudioElement.setAttribute('src', 'media/c.wav');
+//audioElement.load()
+$.get();
+cAudioElement.addEventListener("load", function() {
+    cAudioElement.play();
+}, true);
+$('.c').click(function() {
+    cAudioElement.play();
+});
+// $('.d').click(function() {
+//     audioElement.pause();
+// });
+var dAudioElement = document.createElement('audio');
+dAudioElement.setAttribute('src', 'media/d.wav');
+//audioElement.load()
+$.get();
+dAudioElement.addEventListener("load", function() {
+    dAudioElement.play();
+}, true);
+$('.d').click(function() {
+    dAudioElement.play();
+});
+
+var eAudioElement = document.createElement('audio');
+eAudioElement.setAttribute('src', 'media/e.wav');
+//audioElement.load()
+$.get();
+eAudioElement.addEventListener("load", function() {
+    eAudioElement.play();
+}, true);
+$('.e').click(function() {
+    eAudioElement.play();
+});
+
+var fAudioElement = document.createElement('audio');
+fAudioElement.setAttribute('src', 'media/f.wav');
+//audioElement.load()
+$.get();
+fAudioElement.addEventListener("load", function() {
+    fAudioElement.play();
+}, true);
+$('.f').click(function() {
+    fAudioElement.play();
+});
+
+var gAudioElement = document.createElement('audio');
+gAudioElement.setAttribute('src', 'media/g.wav');
+//audioElement.load()
+$.get();
+gAudioElement.addEventListener("load", function() {
+    gAudioElement.play();
+}, true);
+$('.g').click(function() {
+    gAudioElement.play();
+});
+
+var aAudioElement = document.createElement('audio');
+aAudioElement.setAttribute('src', 'media/a.wav');
+//audioElement.load()
+$.get();
+aAudioElement.addEventListener("load", function() {
+    aAudioElement.play();
+}, true);
+$('.a').click(function() {
+    aAudioElement.play();
+});
+
+var bAudioElement = document.createElement('audio');
+bAudioElement.setAttribute('src', 'media/b.wav');
+//audioElement.load()
+$.get();
+bAudioElement.addEventListener("load", function() {
+    bAudioElement.play();
+}, true);
+$('.b').click(function() {
+    bAudioElement.play();
+});
+
+var c1AudioElement = document.createElement('audio');
+c1AudioElement.setAttribute('src', 'media/c1.wav');
+//audioElement.load()
+$.get();
+c1AudioElement.addEventListener("load", function() {
+    c1AudioElement.play();
+}, true);
+$('.c1').click(function() {
+    c1AudioElement.play();
+});
 
 var round1melody = ["c", "e", "g"];
 var round2melody = ["g", "b", "c1"];
@@ -436,6 +436,83 @@ var nextFourToFiveBtn = $("<button type='button' id='next' name='next'>Next Roun
       console.log("sorry no")
     }
   });
+
+  $(document).keydown(function(e){
+      if (e.keyCode == 65) {
+         cAudioElement.play()
+         $('.c').effect("highlight", {color:"red"}, 500);
+
+         return false;
+      }
+  });
+
+  $(document).keydown(function(e){
+      if (e.keyCode == 83) {
+         dAudioElement.play()
+         $('.d').effect("highlight", {color:"orange"}, 500);
+
+         return false;
+      }
+  });
+  $(document).keydown(function(e){
+      if (e.keyCode == 68) {
+         eAudioElement.play()
+         $('.e').effect("highlight", {color:"yellow"}, 500);
+
+         return false;
+      }
+  });
+  $(document).keydown(function(e){
+      if (e.keyCode == 70) {
+         fAudioElement.play()
+         $('.f').effect("highlight", {color:"green"}, 500);
+
+         return false;
+      }
+  });
+  $(document).keydown(function(e){
+      if (e.keyCode == 71) {
+         gAudioElement.play()
+         $('.g').effect("highlight", {color:"blue"}, 500);
+
+         return false;
+      }
+  });
+  $(document).keydown(function(e){
+      if (e.keyCode == 72) {
+         aAudioElement.play()
+         $('.a').effect("highlight", {color:"purple"}, 500);
+
+         return false;
+      }
+  });
+  $(document).keydown(function(e){
+      if (e.keyCode == 74) {
+         bAudioElement.play()
+         $('.b').effect("highlight", {color:"rgb(227, 0, 255)"}, 500);
+
+         return false;
+      }
+  });
+  $(document).keydown(function(e){
+      if (e.keyCode == 75) {
+         c1AudioElement.play()
+         $('.c1').effect("highlight", {color:"rgb(0, 255, 33)"}, 500);
+
+         return false;
+      }
+  });
+
+
+
+
+//   $(function() {
+//    $(window).keypress(function(a) {
+//        var key = a.which;
+//        cAudioElement.play();
+//
+//    });
+// });
 
   // // next round button functionality
   // $("#next").click(function(event){
