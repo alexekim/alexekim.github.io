e3f0e571799858c9851a7a940f2d1cd423a83df0
Alex Kim <alexkim@macs-MacBook-Pro-2.local>
2019-02-08 16:17:24 -0600

    added react description and animation

--- scripts/components/container.js
+++ scripts/components/container.js
@@ -3,13 +3,34 @@
 class Component extends React.Component{
   constructor(props) {
     super(props);
+    this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
+    this.handleOnMouseLeave = this.handleOnMouseLeave.bind(this);
+  }
+  handleOnMouseOver(){
+    document.getElementById('poweredByReact').style.display = "block";
+  }
+  handleOnMouseLeave(){
+    document.getElementById('poweredByReact').style.display = "none";
   }
   render(){
     return(
-        <div className="container">
+        <div onMouseOver={this.handleOnMouseOver} onMouseLeave={this.handleOnMouseLeave} className="container">
           <div className="row">
-            <div className="col s12">
-            <h2>Web Developer &amp; Cellist</h2>
+            <div className="col s12 center">
+            <h2>Web Developer</h2>
+            </div>
+          </div>
+          <p id="poweredByReact" className="center">
+            made with <i class="fab fa-react"></i> React.js!
+          </p>
+          <div className="row">
+            <div className="col s12 center">
+            <p>
+              I am a creative type that makes sure to enjoy what I do. I've found joy in engineering, being a cellist 🎻 (there's only a violin emoji), bike mechanic 🚲, and petting dogs 🐶.
+            </p>
+            <p>
+              Currently the guy that does problem-solving, building all things web, and making things work at the American Lung Association. There, I've used a lot of JavaScript--including vanilla, jQuery, and React.
+            </p>
             </div>
           </div>
         </div>
--- scripts/vueApp.js
+++ scripts/vueApp.js
@@ -6,32 +6,36 @@ var app = new Vue({
       {
         id: 1,
         name: "nerd",
-        src: "assets/nerd.png"
+        src: "images/nerd.png"
       },
       {
         id: 2,
         name: "sunglasses",
-        src: "assets/sunglasses.png"
+        src: "images/sunglasses.png"
       }
     ],
     headerLinks: [
       {
         id: 1,
+        title: "LinkedIn",
         href: "https://www.linkedin.com/in/alexekim/",
         icon: "<i class='fab fa-linkedin'></i>"
       },
       {
         id: 2,
+        title: "Github",
         href: "https://github.com/alexekim",
         icon: "<i class='fab fa-github'></i>"
       },
       {
         id: 3,
+        title: "Resume",
         href: "assets/Alex Kim Resume 2019.pdf",
         icon: "<i class='fas fa-file'></i>"
       },
       {
         id: 4,
+        title: "e-mail",
         href: "mailto:alexander.e.kim@gmail.com",
         icon: "<i class='fas fa-envelope-square'></i>"
       }
@@ -41,7 +45,7 @@ var app = new Vue({
   methods :{
     changeEmoji: ()=>{
       document.getElementById("headerEmoji").src = app.emojis[1].src;
-      app.vue = "made with <i class='fab fa-vuejs'></i>.js";
+      app.vue = "made with <i class='fab fa-vuejs'></i>ue.js";
     },
     restoreEmoji: ()=>{
       document.getElementById("headerEmoji").src = app.emojis[0].src;
